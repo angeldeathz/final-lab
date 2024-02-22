0a5ca2e3a4adb3645afd6f0edf4434ff2811c411
angeldeathz <martin.david.dev@gmail.com>
2024-02-22 00:04:22 -0300

    feat: added sonar error

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -26,3 +26,6 @@ export const INITIAL_DETAIL_STATE = {
   mediaType: undefined,
   mediaDetail: undefined,
 };
+
+const password =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
